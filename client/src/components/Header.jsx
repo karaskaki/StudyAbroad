4bddd09a00be4a9fa88a45c256a818bab483d86a
Karaskaki <nik.ranchi7@gmail.com>
2024-07-13 08:28:12 +0530

    Complete sign-up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
-// import { useSelector } from "react-redux";
 
 export default function Header() {
-  //   const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -16,16 +14,8 @@ export default function Header() {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/profile">
-            {/* {currentUser ? (
-              <img
-                src={currentUser.profilePicture}
-                alt="profile"
-                className="h-7 w-7 rounded-full object-cover"
-              />
-            ) : ( */}
+          <Link to="/sign-up">
             <li>Sign In</li>
-            {/* )} */}
           </Link>
         </ul>
       </div>
