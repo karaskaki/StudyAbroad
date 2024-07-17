bdf9b3165113b52ea452fea58bf99057ca0c525f
Karaskaki <nik.ranchi7@gmail.com>
2024-07-17 20:04:50 +0530

    updated signout functionality on backend

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, signin } from "../controllers/auth.controller.js";
+import { signup, signin, signout } from "../controllers/auth.controller.js";
 import { google } from "../controllers/auth.controller.js";
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
-// router.get("/signout", signout);
+router.get("/signout", signout);
 
 export default router;
